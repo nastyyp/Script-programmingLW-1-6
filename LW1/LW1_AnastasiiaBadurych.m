0a8887bc47e28186618c35429a1c02f40d5aaaf6
Anastasiia Badurych <anastasiia.badurych@stud.vilniustech.lt>
2026-09-14 10:06:41 +0300

    Create script for plotting two functions
    Added a simple script to plot two functions with labels.

--- LW1/LW1_AnastasiiaBadurych.m
+++ LW1/LW1_AnastasiiaBadurych.m
@@ -2,3 +2,14 @@
 % Surname: Badurych
 % Group: EDIfu 25/2
 % Date: 14-09-2026
+%
+%Paprastas scriptas
+%
+
+x = 1:32;
+y = x.^2;
+
+plot (x,y 'o-r',x, y/3, 'xb')
+title('two functions')
+xlabel ('x-as')
+ylabel ('F_1 [--]   |    F_2 [--]')
